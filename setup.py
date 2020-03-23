a252193f13a1e0708dec7455e0a5a9a5be70d1a0
Duff <hoffmann.dam@gmail.com>
2020-03-23 11:48:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 sys.path.append(join(dirname(__file__), 'src'))
 
-exec(open(join(dirname(__file__), 'src', 'BrowserMobProxyLibrary', 'version.py')).read())
+exec(open(join(dirname(__file__), 'src', 'BrowserMobProxyLibrary')).read())
 
 DESCRIPTION = """
 BrowserMobProxyLibrary is a Robot Framework library ro interface with BrowserMob Proxy.
